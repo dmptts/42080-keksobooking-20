0e0ea73ccd3fb1012dfed9025201857829db6853
Dmitriy Solovev <dmptts@gmail.com>
2020-07-28 21:33:29 +0300

    Добавляет возможность фильтрации квартир по остальным критериям

--- js/filter.js
+++ js/filter.js
@@ -4,6 +4,10 @@
   var filters = document.querySelector('.map__filters');
   var filterInputs = filters.querySelectorAll('.map__filter, .map__features');
   var filterType = document.querySelector('#housing-type');
+  var filterPrice = document.querySelector('#housing-price');
+  var filterRooms = document.querySelector('#housing-rooms');
+  var filterGuests = document.querySelector('#housing-guests');
+  var filterFeatures = document.querySelector('#housing-features');
 
   var initFilters = function () {
     filters.classList.remove('map__filters--hidden');
@@ -32,11 +36,39 @@
   };
 
   var checkFlat = function (flat) {
-    return (filterByType(flat)); // Здесь позже будут другие функции фильтра
+    return (filterByType(flat) && filterByPrice(flat) && filterByRooms(flat)) && filterByGuests(flat) && filterByFeatures(flat);
   };
 
   var filterByType = function (flat) {
     return flat.offer.type === filterType.value || filterType.value === 'any';
+  };
+
+  var filterByPrice = function (flat) {
+    switch (filterPrice.value) {
+      case 'low':
+        return flat.offer.price < 10000;
+      case 'middle':
+        return flat.offer.price >= 10000 && flat.offer.price < 50000;
+      case 'high':
+        return flat.offer.price >= 50000;
+      default:
+        return true;
+    }
+  };
+
+  var filterByRooms = function (flat) {
+    return parseInt(filterRooms.value, 10) === flat.offer.rooms || filterRooms.value === 'any';
+  };
+
+  var filterByGuests = function (flat) {
+    return parseInt(filterGuests.value, 10) === flat.offer.guests || filterGuests.value === 'any';
+  };
+
+  var filterByFeatures = function (flat) {
+    var checkedFeatures = filterFeatures.querySelectorAll(':checked');
+    return Array.from(checkedFeatures).every(function (checkedFeature) {
+      return flat.offer.features.includes(checkedFeature.value);
+    });
   };
 
   var onFiltersChange = function (evt) {
