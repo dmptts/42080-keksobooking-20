8ee151ce757414a25fe723e619984a6a5e1aa548
Dmitriy Solovev <dmptts@gmail.com>
2020-07-29 00:42:47 +0300

    Изменяет debounce

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,11 @@
 'use strict';
 
 (function () {
+  var DEBOUNCE_INTERVAL = 2000;
+  var LOW_PRICE = 10000;
+  var HIGH_PRICE = 50000;
+  var FILTER_ANY_VALUE = 'any';
+
   var filters = document.querySelector('.map__filters');
   var filterInputs = filters.querySelectorAll('.map__filter, .map__features');
   var filterType = document.querySelector('#housing-type');
@@ -9,10 +14,26 @@
   var filterGuests = document.querySelector('#housing-guests');
   var filterFeatures = document.querySelector('#housing-features');
 
+  var debounce = function (cb) {
+    var lastTimeout = null;
+
+    return function () {
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(function () {
+        cb.apply(null, arguments);
+      }, DEBOUNCE_INTERVAL);
+    };
+  };
+
   var initFilters = function () {
     filters.classList.remove('map__filters--hidden');
     window.form.toggleInputs(filterInputs, false);
-    filters.addEventListener('change', onFiltersChange);
+    filters.addEventListener('change', function (evt) {
+      evt.preventDefault();
+      onFiltersChange();
+    });
   };
 
   var disableFilters = function () {
@@ -40,28 +61,28 @@
   };
 
   var filterByType = function (flat) {
-    return flat.offer.type === filterType.value || filterType.value === 'any';
+    return flat.offer.type === filterType.value || filterType.value === FILTER_ANY_VALUE;
   };
 
   var filterByPrice = function (flat) {
     switch (filterPrice.value) {
       case 'low':
-        return flat.offer.price < 10000;
+        return flat.offer.price < LOW_PRICE;
       case 'middle':
-        return flat.offer.price >= 10000 && flat.offer.price < 50000;
+        return flat.offer.price >= LOW_PRICE && flat.offer.price < HIGH_PRICE;
       case 'high':
-        return flat.offer.price >= 50000;
+        return flat.offer.price >= HIGH_PRICE;
       default:
         return true;
     }
   };
 
   var filterByRooms = function (flat) {
-    return parseInt(filterRooms.value, 10) === flat.offer.rooms || filterRooms.value === 'any';
+    return parseInt(filterRooms.value, 10) === flat.offer.rooms || filterRooms.value === FILTER_ANY_VALUE;
   };
 
   var filterByGuests = function (flat) {
-    return parseInt(filterGuests.value, 10) === flat.offer.guests || filterGuests.value === 'any';
+    return parseInt(filterGuests.value, 10) === flat.offer.guests || filterGuests.value === FILTER_ANY_VALUE;
   };
 
   var filterByFeatures = function (flat) {
@@ -71,21 +92,11 @@
     });
   };
 
-  var onFiltersChange = function (evt) {
-    var lastTimeout;
-
-    evt.preventDefault();
+  var onFiltersChange = debounce(function () {
     window.card.remove();
-
-    if (lastTimeout) {
-      window.clearTimeout(lastTimeout);
-    }
-
-    lastTimeout = window.setTimeout(function () {
-      window.map.removePins();
-      window.map.renderPins(window.map.pinsData);
-    }, 500);
-  };
+    window.map.removePins();
+    window.map.renderPins(window.map.pinsData);
+  });
 
   disableFilters();
 
